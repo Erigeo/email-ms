8b7ac5183ace76d2da0929d753e0465d3d4d3e16
Georg Herison <georgherison@gmail.com>
2024-07-20 22:31:50 -0300

    service implementhed

--- src/main/java/com/georg/ms_email/repositories/EmailRepository.java
+++ src/main/java/com/georg/ms_email/repositories/EmailRepository.java
@@ -1,9 +1,12 @@
 package com.georg.ms_email.repositories;
 
 import com.georg.ms_email.models.Email;
+import org.springframework.data.domain.Page;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.awt.print.Pageable;
 import java.util.UUID;
 
 public interface EmailRepository extends MongoRepository<Email, UUID> {
+    Page<Email> findByReceiver(String receiver, Pageable pageable);
 }
--- src/main/java/com/georg/ms_email/services/EmailService.java
+++ src/main/java/com/georg/ms_email/services/EmailService.java
@@ -6,14 +6,14 @@ import com.georg.ms_email.models.Email;
 import com.georg.ms_email.repositories.EmailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
-
-import java.awt.print.Pageable;
-import java.time.LocalDate;
+import org.springframework.data.domain.Pageable;
 import java.time.LocalDateTime;
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class EmailService {
@@ -44,16 +44,21 @@ public class EmailService {
         }
     }
 
-    public Optional<Email> findByUUID(String uuid) {
-
+    public Optional<Email> findByUUID(UUID uuid) {
+        return emailRepository.findById(uuid);
     }
 
-    public Page<Email> findByReceiver(String receiver) {
+   // public Page<Email> findByReceiver(String receiver, int page, int size) {
+     //   Pageable pageable = PageRequest.of(page, size);
+      //  return emailRepository.findByReceiver(receiver, (java.awt.print.Pageable) pageable);
+    //}
 
+    public Page<Email> findByReceiver(String receiver, Pageable pageable) {
+        return emailRepository.findByReceiver(receiver, (java.awt.print.Pageable) pageable);
     }
 
     public Page<Email> findAll(Pageable pageable) {
-
+        return emailRepository.findAll(pageable);
     }
 
 
